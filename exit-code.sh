baa9da65f1e358de250039689e619d28a24975c6
Mehrdad Ghasemkhani <m.ghasemkhani@spara.ir>
2021-08-01 17:36:34 +0430

    Add 'exit-code' & 'read'

---
+++ exit-code.sh
@@ -0,0 +1,6 @@
+exit n 
+
+0           success
+1 - 255     specific error code
+126         not executable file
+127         no such command
---
+++ read.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+read -s     does not echo input
+read -nN    accepts only N characters of input
+read -p     propmt message
+read -tT    accepts input for T second
