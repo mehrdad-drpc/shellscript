b7b8f191d2c043a5faaac4e40fa2b227d4c151d1
Mehrdad Ghasemkhani <m.ghasemkhani@spara.ir>
2021-08-01 11:52:39 +0430

    Add another for loop style

--- forloop.sh
+++ forloop.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-for ((i = 1; i <= 10; i++)); do
-    echo "I is $i"
+# C style
+for ((c = 1; c <= 10; c++)); do
+    echo "C is $c"
+done
+
+echo "-------------"
+
+# python style
+for p in {1..10};do
+    echo "P is $p"
 done
