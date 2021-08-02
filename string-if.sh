faa2e22b5926b60a729e9b69ed77089e15431535
Mehrdad <mehrdad.drpc@gmail.com>
2021-08-02 15:51:04 +0430

    Add if str & int

---
+++ string-if.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+testuser=mehrdad
+
+# if test $testuser = mehrdad; then
+#     echo "true"
+# else
+#     echo "false"
+# fi
+
+
+# str1 =  str2 
+# str1 != str2
+# str1 < str1
+# str1 > str1
+# -n    check str if greater than zero
+# -z    check str if equel to zero
+
+str1="A A"
+if [ "$str1" = "A A" ]; then
+    echo "true"
+else
+    echo "false"
+fi
