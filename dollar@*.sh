37881ded1cc33b96e015c33b0e20b0500cc1f2b9
Mehrdad <mehrdad.drpc@gmail.com>
2021-08-05 01:07:02 +0430

    Add  vs @* defferenceses

---
+++ dollar@*.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+count=1
+for param in "$*"
+do
+    echo "\$* parameters #$count = $param"
+    count=$[ count + 1 ]
+done
+
+echo "########################################"
+
+count=1
+for param in "$@"
+do
+    echo "\$* parameters #$count = $param"
+    count=$[ count + 1 ]
+done
