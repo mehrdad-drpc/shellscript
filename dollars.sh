2b05464b751e118c389ba53754f22d68929e7ef6
Mehrdad <mehrdad.drpc@gmail.com>
2021-08-05 00:58:05 +0430

    Add basename & rename count to dollars

---
+++ dollars.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "your script name is $0"
+echo "your first parameter is $1"
+echo "your second parameter is $2"
+
+echo "you entered $# input parameter"
+
+echo "'$*' is $*"
+echo "'$@' is $@"
