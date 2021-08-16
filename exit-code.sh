957b235c1526bf43235f56e325b8673a9fc649c0
Mehrdad Ghasemkhani <m.ghasemkhani@spara.ir>
2021-08-16 11:20:32 +0430

    Add read command md file

--- exit-code.sh
+++
@@ -1,6 +0,0 @@
-exit n 
-
-0           success
-1 - 255     specific error code
-126         not executable file
-127         no such command
--- read.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-read -s     does not echo input
-read -nN    accepts only N characters of input
-read -p     propmt message
-read -tT    accepts input for T second
