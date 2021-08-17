f4d60f89a10eb144d18e841b4fb945583e8e98c8
Mehrdad Ghasemkhani <m.ghasemkhani@spara.ir>
2021-08-17 13:55:36 +0430

    Update fi_string.sh file

--- code/if_string.sh
+++ code/if_string.sh
@@ -16,9 +16,16 @@ testuser=mehrdad
 # -n    check str if greater than zero
 # -z    check str if equel to zero
 
-str1="A A"
-if [ "$str1" = "A A" ]; then
-    echo "true"
-else
-    echo "false"
-fi
+# str1="A A"
+# if [ "$str1" = "A A" ]; then
+#     echo "true"
+# else
+#     echo "false"
+# fi
+
+read -p "enter a name: " str
+while [ -n $str ]   # OR while [[ $str != "" ]]
+do
+    echo "yes $str"
+    read str
+done
